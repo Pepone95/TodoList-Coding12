f9548624829247641383ff6973a0f7786656a90b
Younes <faouzi.younes@hotmail.com>
2020-02-04 14:14:02 +0100

    Boutton ajouter

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+let body = document.querySelector("body")
+let input1 = document.getElementsByClassName("form-control")[0]
+
+// LE  BOUTTON AJOUTER
+let Ajouter = document.getElementsByClassName("btn")[0]
+Ajouter.style.backgroundColor = 'turquoise'
