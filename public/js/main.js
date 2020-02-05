e691a04e83b7690ecccd455df7371ba9ef0ad2cb
Younes <faouzi.younes@hotmail.com>
2020-02-05 11:31:21 +0100

    modification du boutton ajouter

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ let input1 = document.getElementsByClassName("form-control")[0]
 
 let Ajouter = document.getElementsByClassName("btn")[0]
 Ajouter.style.backgroundColor = 'turquoise'
+Ajouter.style.marginRight = '200px'
 
 // 4 boutton
 
@@ -32,27 +33,32 @@ Ajouter.addEventListener("click", () => {
     li.style.display = 'flex'
     li.style.alignItems = 'center'
     li.style.justifyContent = "space-between"
-    li.style.paddingLeft = '35px'
+    li.style.padding = '25px'
     li.style.fontWeight = 'bold'
     li.style.backgroundColor = "lightgray"
 
+    // les logo boutton
+
     let div = document.createElement('div')
+    li.appendChild(div)
 
     let btni = document.createElement('i')
     btni.setAttribute("class", "far fa-2x fa-check-circle")
-    btni.style.marginLeft = '750px'
+    btni.style.color = 'blue'
     div.appendChild(btni)
 
     let btne = document.createElement('i')
     btne.setAttribute("class", "fas fa-2x fa-edit")
     btne.style.marginLeft = '50px'
+    btne.style.color = 'yellow'
     div.appendChild(btne)
-    li.appendChild(div)
-
+    
     let btns = document.createElement('i')
     btns.setAttribute("class", "far fa-2x fa-trash-alt")
     btns.style.marginLeft = '50px'
+    btns.style.color = 'red'
     div.appendChild(btns)
-    li.appendChild(div)
+    //  fin des logo boutton
+    
   }
 })
