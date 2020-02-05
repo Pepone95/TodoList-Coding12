a0e19e8cb951955a6f35ead732a4a7dea02a0215
Younes <faouzi.younes@hotmail.com>
2020-02-05 09:19:22 +0100

    2eme partie js

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,58 @@
 let body = document.querySelector("body")
 let input1 = document.getElementsByClassName("form-control")[0]
 
-// LE  BOUTTON AJOUTER
+// Boutton ajouter
 
 let Ajouter = document.getElementsByClassName("btn")[0]
 Ajouter.style.backgroundColor = 'turquoise'
 
-// les 4 BUTTONS 
+// 4 boutton
 
 let buttonTodo = document.getElementsByClassName("btn")[1]
-​
+
 let buttonDone = document.getElementsByClassName("btn")[2]
-​
+
 let supprimer = document.getElementsByClassName("btn")[3]
 let buttonAll = document.getElementsByClassName("btn")[4]
-​
+
 let boxBody = document.getElementsByClassName("box-body")[0]
-​
+
 let ul = document.getElementById("list all")
 boxBody.appendChild(ul)
-​
+
 Ajouter.addEventListener("click", () => {
   if(input1.value !== ""){
     let li = document.createElement("li")
     li.innerText = input1.value
     input1.value = ""
     ul.appendChild(li)
-    // STYLE Li
+
+    // Les Li
+
     li.style.display = 'flex'
     li.style.alignItems = 'center'
+    li.style.justifyContent = "space-between"
     li.style.paddingLeft = '35px'
-    li.style.fontWeight = 'bold' 
-    li.style.width = "1100px"
-    li.style.height = "100px"
+    li.style.fontWeight = 'bold'
     li.style.backgroundColor = "lightgray"
-    li.style.borderRadius = "1%"
-    
+
+    let div = document.createElement('div')
+
+    let btni = document.createElement('i')
+    btni.setAttribute("class", "far fa-2x fa-check-circle")
+    btni.style.marginLeft = '750px'
+    div.appendChild(btni)
+
+    let btne = document.createElement('i')
+    btne.setAttribute("class", "fas fa-2x fa-edit")
+    btne.style.marginLeft = '50px'
+    div.appendChild(btne)
+    li.appendChild(div)
+
+    let btns = document.createElement('i')
+    btns.setAttribute("class", "far fa-2x fa-trash-alt")
+    btns.style.marginLeft = '50px'
+    div.appendChild(btns)
+    li.appendChild(div)
   }
 })
