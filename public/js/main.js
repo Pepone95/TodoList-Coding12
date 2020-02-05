107cc76b1096eb19cbfde549f40bf4b7a2867070
Younes <faouzi.younes@hotmail.com>
2020-02-05 20:05:23 +0100

    rajout le btn save

--- public/js/main.js
+++ public/js/main.js
@@ -24,9 +24,9 @@ boxBody.appendChild(ul)
 Ajouter.addEventListener("click", () => {
   if(input1.value !== ""){
     let li = document.createElement("li")
+    ul.appendChild(li)
     li.innerText = input1.value
     input1.value = ""
-    ul.appendChild(li)
 
     // Les Li
 
@@ -37,7 +37,7 @@ Ajouter.addEventListener("click", () => {
     li.style.fontWeight = 'bold'
     li.style.backgroundColor = "lightgray"
 
-    // les logo boutton
+    // BTN CHECK
 
     let div = document.createElement('div')
     li.appendChild(div)
@@ -46,19 +46,57 @@ Ajouter.addEventListener("click", () => {
     btni.setAttribute("class", "far fa-2x fa-check-circle")
     btni.style.color = 'blue'
     div.appendChild(btni)
+    
+    
+    btni.addEventListener("click", () => {
+      if (li.style.backgroundColor == "green"){
+        li.style.backgroundColor = "lightgray"
+      } else {
+        li.style.backgroundColor = "green"
+      }
+    })
+    //  FIN DE BTN CHECK
+    
 
+    //  BTN EDIT
     let btne = document.createElement('i')
     btne.setAttribute("class", "fas fa-2x fa-edit")
     btne.style.marginLeft = '50px'
     btne.style.color = 'yellow'
     div.appendChild(btne)
+
+    btne.addEventListener("click", () => {
+      btni.remove()
+      btnd.remove()
+      btne.remove()
+      let input2 = document.createElement("input")
+      li.appendChild(input2)
+      let btns = document.createElement('i')
+      btns.setAttribute("class", "fas fa-2x fa-save")
+      btns.style.color = "green"
+      btns.style.marginRight = "650px"
+      li.appendChild(btns)
+      // click sur le save => rajoute les 3 bouton de base check,edit,delete
+      btns.addEventListener("click", () => {
+        li.innerText = input2.value
+        li.append(btni, btne, btnd)
+
+        
+      })
+
+
+      
+    })
+
+    // FIN BTN EDIT
+
     
-    let btns = document.createElement('i')
-    btns.setAttribute("class", "far fa-2x fa-trash-alt")
-    btns.style.marginLeft = '50px'
-    btns.style.color = 'red'
-    div.appendChild(btns)
+    let btnd = document.createElement('i')
+    btnd.setAttribute("class", "far fa-2x fa-trash-alt")
+    btnd.style.marginLeft = '50px'
+    btnd.style.color = 'red'
+    div.appendChild(btnd)
     //  fin des logo boutton
-    
+
   }
 })
